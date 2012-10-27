05afdb2efc13c508038a406ecbab43a9bb122982
Adrian Vasile <adrian.vasile@sparktech.ro>
2012-10-27 19:44:27 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundpathApi::Application.config.secret_token = '4dce9def7129faaeccbbc39049094514b137c9385c1aa1d479b592b7ab16b3bd0405effc9cd3af9539bb0ade175c5ed7aee30945912afe4f4e4bf3b0b2900dc0'
